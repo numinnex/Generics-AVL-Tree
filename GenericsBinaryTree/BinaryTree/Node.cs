82f43423166bf16b585b63b7095e4e11b9453807
NUM1NEX <numinex767@gmail.com>
2022-09-06 15:13:07 +0200

    Create Node Class

---
+++ GenericsBinaryTree/BinaryTree/Node.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsBinaryTree.BinaryTree
+{
+    public class Node<T>
+    {
+        public T Data { get; private set; }
+        public Node(T data)
+        {
+            Data = data;
+        }   
+
+        public Node<T> ? Parent { get; set; }
+        public Node<T>? Left { get; set; }
+        public Node<T>? Right { get; set; }
+
+    }
+
+
+}
