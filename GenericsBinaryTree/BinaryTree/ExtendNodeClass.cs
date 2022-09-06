e1345353326353d4648a43a89e0111625f3d1834
NUM1NEX <numinex767@gmail.com>
2022-09-06 15:43:57 +0200

    Create node extension class

--- GenericsBinaryTree/BinaryTree/ExtendNodeClass.cs
+++ GenericsBinaryTree/BinaryTree/ExtendNodeClass.cs
@@ -1,21 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GenericsBinaryTree.BinaryTree
+﻿namespace GenericsBinaryTree.BinaryTree
 {
     public static class ExtendNodeClass
     {
-        public static Node<T> Maximum(this Node<T> tree)
+        public static Node<T> Maximum<T>(this Node<T> tree)
         {
             Node<T> current = tree;
-            while(current.Right != null)
+            while (current.Right != null)
             {
                 current = current.Right;
             }
             return current;
         }
+
+        public static Node<T> Minimum<T>(this Node<T> tree)
+        {
+            Node<T> current = tree;
+            while (current.Left != null)
+            {
+                current = current.Left;
+            }
+            return current;
+        }
+
+        public static Node<T> Successor<T>(this Node<T> node)
+        {
+            if(node.Right != null)
+                return node.Right.Minimum();
+
+            Node<T>? current = node.Parent;
+
+            while(current != null && node == current.Right)
+            {
+                node = current;
+                current = current.Parent;
+            }
+
+            return current;
+
+        }
+        public static Node<T> Predecessor<T>(this Node<T> node)
+        {
+            if (node.Left != null)
+            {
+                return node.Left.Maximum();
+            }
+
+            Node<T>? current = node.Parent;
+
+            while (current != null && node == current.Left)
+            {
+                node = current;
+                current = current.Parent;
+            }
+
+            return current;
+        }
     }
 }
+
